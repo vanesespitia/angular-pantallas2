0cc154cc36acb453735aee73c6ce05c8f332d381
vanesespitia <vaneespitiagv@gmail.com>
2019-11-21 07:49:34 -0600

    2 files modified, 3 added, 1 deleted

--- src/app/L/hello.component.ts
+++ src/app/L/hello.component.ts
@@ -2,8 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  templateUrl: './l.html',
+  styleUrls: ['./login.component.css']
 })
 export class HelloComponent  {
   @Input() name: string;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HelloComponent } from './L/hello.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
